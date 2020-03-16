600ca429b384c90e136c33e25543a27dcfac4db5
dave1496 <dav_elmaleh@hotmail.com>
2020-03-17 00:21:57 +0100

    fix

--- messages/MessagePerformative.py
+++ messages/MessagePerformative.py
@@ -14,13 +14,13 @@ import enum
 class MessagePerformative(enum.Enum):
     """MessagePerformative class.
     """
-    # Those who needs an answer
+    # Those who need an answer
     PROPOSE = 1
     ASK_WHY = 3
     QUERY_REF = 5
     COMMIT = 7
     ARGUE = 9
-    # Those who does not need an answer
+    # Those who do not need an answer
     ACCEPT = 2
     INFORM_REF = 4
     TAKE = 6
